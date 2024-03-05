9e4a23df468901135c2efca059bffc1712f098ba
andreineagoe23 <you@example.com>
2024-03-05 12:33:31 +0000

    .

--- weather-app/src/Footer.js
+++ weather-app/src/Footer.js
@@ -1,26 +1,36 @@
 // Footer.js
-import React from "react";
-import "./Footer.css"; // Create this file next
+import React, { useState } from "react";
+import "./Footer.css";
 import { ReactComponent as HomeButton } from "./HomeButton.svg";
 import { ReactComponent as Plant } from "./plant.svg";
 import { ReactComponent as BurgerMenu } from "./BurgerMenu.svg";
 
 function Footer() {
+  const [activeButton, setActiveButton] = useState(null);
+  const handleClick = (buttonIndex) => {
+    setActiveButton(buttonIndex);
+  };
+
   return (
     <footer className="app-footer">
-      {
-        <>
-          <button className="footer-button">
-            <Plant />
-          </button>
-          <button className="footer-button">
-            <HomeButton />
-          </button>
-          <button className="footer-button">
-            <BurgerMenu />
-          </button>
-        </>
-      }
+      <button
+        className={`footer-button ${activeButton === 0 ? "active" : ""}`}
+        onClick={() => handleClick(0)}
+      >
+        <Plant />
+      </button>
+      <button
+        className={`footer-button ${activeButton === 1 ? "active" : ""}`}
+        onClick={() => handleClick(1)}
+      >
+        <HomeButton />
+      </button>
+      <button
+        className={`footer-button ${activeButton === 2 ? "active" : ""}`}
+        onClick={() => handleClick(2)}
+      >
+        <BurgerMenu />
+      </button>
     </footer>
   );
 }
