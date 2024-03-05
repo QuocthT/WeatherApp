f8a48f60ef5167ab5d2834fc146c2469ab349e02
andreineagoe23 <you@example.com>
2024-03-05 12:13:39 +0000

    .

--- weather-app/src/Footer.js
+++ weather-app/src/Footer.js
@@ -1,15 +1,24 @@
 // Footer.js
 import React from "react";
 import "./Footer.css"; // Create this file next
+import { ReactComponent as HomeButton } from "./HomeButton.svg";
+import { ReactComponent as Plant } from "./plant.svg";
+import { ReactComponent as BurgerMenu } from "./BurgerMenu.svg";
 
 function Footer() {
   return (
     <footer className="app-footer">
       {
         <>
-          <button className="footer-button">Button 1</button>
-          <button className="footer-button">Button 2</button>
-          <button className="footer-button">Button 3</button>
+          <button className="footer-button">
+            <Plant />
+          </button>
+          <button className="footer-button">
+            <HomeButton />
+          </button>
+          <button className="footer-button">
+            <BurgerMenu />
+          </button>
         </>
       }
     </footer>
