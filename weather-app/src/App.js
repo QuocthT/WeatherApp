f0a49f94b48db43d1012c2d77a8f795e2df544e3
andreineagoe23 <you@example.com>
2024-03-11 16:45:52 +0000

    .

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./Footer.js";
 import EightDayForecast from "./EightDayForecast.js";
@@ -6,20 +5,7 @@ import EightDayForecast from "./EightDayForecast.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
       <EightDayForecast />
       <Footer />
     </div>
