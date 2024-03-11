62bdc76a941244456a2bee70654fc177cacf15da
andreineagoe23 <you@example.com>
2024-03-11 19:44:52 +0000

    .

---
+++ weather-app/src/CurrentWeather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CurrentWeather.css";
+
+const CurrentWeather = () => {
+  return (
+    <div className="current-weather">
+      {/* Content for current weather will go here */}
+    </div>
+  );
+};
+
+export default CurrentWeather;
