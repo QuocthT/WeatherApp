a6428f0a9df7df5a76f368a9f3fca8f1ece89982
ec22815 <ec22815@qmul.ac.uk>
2024-03-13 16:23:39 +0000

    zep

---
+++ weather-app/src/LeftPage.js
@@ -0,0 +1,13 @@
+// LeftPage.js
+import React from "react";
+import "./LeftPage.css";
+
+const LeftPage = () => {
+  return (
+    <div className="left-page-container">
+      {/* The content from your previous code goes here */}
+    </div>
+  );
+};
+
+export default LeftPage;
