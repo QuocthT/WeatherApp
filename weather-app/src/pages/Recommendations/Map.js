aa1a4f7162f44c316bdf09cc52c2c7d5045c42df
ec22815 <ec22815@qmul.ac.uk>
2024-03-17 14:32:56 +0000

    map map

--- weather-app/src/pages/Recommendations/Map.js
+++ weather-app/src/pages/Recommendations/Map.js
@@ -7,17 +7,18 @@ const Map = () => {
 
   useEffect(() => {
     const apiKey = '9dd77ed418cc13a9dea204f8b91aaf61';
-    const op = 'clouds_new';
-    const z = 10;
-    const x = 0;
-    const y = 0;
-    const mapUrl = `http://maps.openweathermap.org/maps/2.0/weather/${op}/${z}/${x}/${y}?appid=${apiKey}`;
+    const op = 'PA0';
+    const z = '{z}';
+    const x = '{x}';
+    const y = '{y}';
+    const date = '1552861800'; // Example date, you can change it as needed
+    const mapUrl = `http://maps.openweathermap.org/maps/2.0/weather/${op}/${z}/${x}/${y}?date=${date}&appid=${apiKey}`;
     setMapUrl(mapUrl);
   }, []);
 
   return (
     <div className="map-container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <MapContainer center={[52.3555, -1.1743]} zoom={6} style={{ height: '600px', width: '100%' }}>
         <TileLayer url={mapUrl} />
       </MapContainer>
     </div>
