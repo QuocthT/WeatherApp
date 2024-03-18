9dab4849e557039572a8fb39747c9c111ae5f93f
ec22815 <ec22815@qmul.ac.uk>
2024-03-18 11:23:36 +0000

    s

--- weather-app/src/pages/Recommendations/Map.js
+++ weather-app/src/pages/Recommendations/Map.js
@@ -12,7 +12,7 @@ const Map = () => {
     const x = '{x}';
     const y = '{y}';
     const fillBound = true;
-    const opacity = 0.6;
+    const opacity = 0.1;
     const palette = '-65:821692;-55:821692;-45:821692;-40:821692;-30:8257db;-20:208cec;-10:20c4e8;0:23dddd;10:c2ff28;20:fff028;25:ffc228;30:fc8014';
     const mapUrl = `http://maps.openweathermap.org/maps/2.0/weather/${op}/${z}/${x}/${y}?appid=${apiKey}&fill_bound=${fillBound}&opacity=${opacity}&palette=${palette}`;
     setMapUrl(mapUrl);
