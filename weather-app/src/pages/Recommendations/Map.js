ffe0bed8c712b3f79de3c52018b34463c57dcfd4
nirosh7 <officialnirosh@gmail.com>
2024-03-20 17:36:24 +0000

    css for left page

--- weather-app/src/pages/Recommendations/Map.js
+++ weather-app/src/pages/Recommendations/Map.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const Map = () => {
   const [map, setMap] = useState(null);
+  const [weatherOverlay, setWeatherOverlay] = useState(null);
 
   useEffect(() => {
     if (!map) {
@@ -12,6 +13,24 @@ const Map = () => {
       setMap(mapInstance);
     }
   }, [map]);
+
+  useEffect(() => {
+    if (map && !weatherOverlay) {
+      // Add weather overlay here
+      const apiKey = '82a5bdefb47889cf36ac199b655b5d38';
+      const weatherTileUrl = `http://maps.openweathermap.org/maps/2.0/weather/PA0/{z}/{x}/{y}?date=1552861800&appid=$82a5bdefb47889cf36ac199b655b5d38`;
+      const bounds = new window.google.maps.LatLngBounds(
+        new window.google.maps.LatLng(85, -180),
+        new window.google.maps.LatLng(-85, 180)
+      );
+      const weatherOverlayInstance = new window.google.maps.GroundOverlay(
+        weatherTileUrl,
+        bounds
+      );
+      weatherOverlayInstance.setMap(map);
+      setWeatherOverlay(weatherOverlayInstance);
+    }
+  }, [map, weatherOverlay]);
 
   return (
     <div className="map-container" id="map" style={{ height: '600px', width: '100%' }}>
