49d18efd8a1cd8411ff61a12cbf63a21faeece43
ec22815 <ec22815@qmul.ac.uk>
2024-03-21 00:45:28 +0000

    ch

--- weather-app/src/pages/Recommendations/Map.js
+++ weather-app/src/pages/Recommendations/Map.js
@@ -1,51 +1,47 @@
 import React, { useEffect, useState } from 'react';
 
 const Map = () => {
-  // State variables to hold the map instance and weather overlay instance
+  // Declares the state variables and its setter functions
+  // These are state variables for map and weather overlay
   const [map, setMap] = useState(null);
   const [weatherOverlay, setWeatherOverlay] = useState(null);
 
-  // Effect hook to initialize the map instance
+  // This initializes the map instance
   useEffect(() => {
-    // Check if the map instance doesn't exist
+    // This checks if the map instance doesn't exist
     if (!map) {
-      // Create a new Google Maps instance and set it to state
+      // This create a new Google Maps instance
       const mapInstance = new window.google.maps.Map(document.getElementById('map'), {
         center: { lat: 52.3555, lng: -1.1743 },
         zoom: 6,
       });
       setMap(mapInstance);
     }
-  }, [map]); // Dependency array ensures this effect runs only when map state changes
+  }, [map]);
 
-  // Effect hook to add the weather overlay to the map
+  // This puts a weather overlay on the map instance
   useEffect(() => {
-    // Check if both map instance and weather overlay instance exist
     if (map && !weatherOverlay) {
-      // API key for OpenWeatherMap
       const apiKey = '82a5bdefb47889cf36ac199b655b5d38';
-      // URL for fetching weather tiles
       const weatherTileUrl = `http://maps.openweathermap.org/maps/2.0/weather/PA0/{z}/{x}/{y}?date=1552861800&appid=$82a5bdefb47889cf36ac199b655b5d38`;
-      // Define the bounds for the weather overlay
+      // This defines the bounds for the weather overlay
       const bounds = new window.google.maps.LatLngBounds(
         new window.google.maps.LatLng(85, -180),
         new window.google.maps.LatLng(-85, 180)
       );
-      // Create a new GroundOverlay instance for weather tiles
       const weatherOverlayInstance = new window.google.maps.GroundOverlay(
         weatherTileUrl,
         bounds
       );
-      // Set the weather overlay on the map
+      // This puts the weather overlay on the map
       weatherOverlayInstance.setMap(map);
-      // Set the weather overlay instance to state
       setWeatherOverlay(weatherOverlayInstance);
     }
-  }, [map, weatherOverlay]); // Dependency array ensures this effect runs when map or weatherOverlay state changes
+  }, [map, weatherOverlay]);
 
   return (
     <div className="map-container" id="map" style={{ height: '600px', width: '100%' }}>
-      {/* Map will be rendered here */}
+      {/* Map will be shown here */}
     </div>
   );
 };
